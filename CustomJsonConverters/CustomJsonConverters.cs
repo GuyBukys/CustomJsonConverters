002e22769fad92a02ac3e0f65caf95fbfed500c5
GuyBuky <guybuky5390@gmail.com>
2022-02-09 21:23:28 +0200

    changed methods to lambda functions

--- CustomJsonConverters/CustomJsonConverters.cs
+++ CustomJsonConverters/CustomJsonConverters.cs
@@ -5,23 +5,11 @@ namespace CustomJsonConverters;
 
 public static class CustomJsonConverters
 {
-    public static JsonConverter StringToDateOnlyConverter()
-    {
-        return new StringToDateOnlyConverter();
-    }
+    public static JsonConverter StringToDateOnlyConverter() => new StringToDateOnlyConverter();
 
-    public static JsonConverter StringToDateOnlyConverter(string dateFormat)
-    {
-        return new StringToDateOnlyConverter(dateFormat);
-    }
+    public static JsonConverter StringToDateOnlyConverter(string dateFormat) => new StringToDateOnlyConverter(dateFormat);
 
-    public static JsonConverter StringToTimeOnlyConverter()
-    {
-        return new StringToTimeOnlyConverter();
-    }
+    public static JsonConverter StringToTimeOnlyConverter() => new StringToTimeOnlyConverter();
 
-    public static JsonConverter StringToTimeOnlyConverter(string timeFormat)
-    {
-        return new StringToTimeOnlyConverter(timeFormat);
-    }
+    public static JsonConverter StringToTimeOnlyConverter(string timeFormat) => new StringToTimeOnlyConverter(timeFormat);
 }

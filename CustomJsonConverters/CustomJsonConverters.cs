fac6096fbf4988618a8863ba76bdf1a64d566cb3
GuyBuky <guybuky5390@gmail.com>
2022-02-09 21:31:08 +0200

    added documentation

--- CustomJsonConverters/CustomJsonConverters.cs
+++ CustomJsonConverters/CustomJsonConverters.cs
@@ -3,13 +3,34 @@ using System.Text.Json.Serialization;
 
 namespace CustomJsonConverters;
 
+/// <summary>
+/// static class for creating custom json converter object.
+/// </summary>
 public static class CustomJsonConverters
 {
+    /// <summary>
+    /// <see cref="string"/> to <see cref="DateOnly"/> converter.
+    /// </summary>
+    /// <returns>The custom <see cref="JsonConverter{DateOnly}"/> instance</returns>
     public static JsonConverter StringToDateOnlyConverter() => new StringToDateOnlyConverter();
 
+    /// <summary>
+    /// <see cref="string"/> to <see cref="DateOnly"/> converter.
+    /// </summary>
+    /// <param name="dateFormat">The date string format.</param>
+    /// <returns>The custom <see cref="JsonConverter{DateOnly}"/> instance</returns>
     public static JsonConverter StringToDateOnlyConverter(string dateFormat) => new StringToDateOnlyConverter(dateFormat);
 
+    /// <summary>
+    /// <see cref="string"/> to <see cref="TimeOnly"/> converter.
+    /// </summary>
+    /// <returns>The custom <see cref="JsonConverter{TimeOnly}"/> instance</returns>
     public static JsonConverter StringToTimeOnlyConverter() => new StringToTimeOnlyConverter();
 
+    /// <summary>
+    /// <see cref="string"/> to <see cref="TimeOnly"/> converter.
+    /// </summary>
+    /// <param name="timeFormat">The time string format.</param>
+    /// <returns>The custom <see cref="JsonConverter{TimeOnly}"/> instance</returns>
     public static JsonConverter StringToTimeOnlyConverter(string timeFormat) => new StringToTimeOnlyConverter(timeFormat);
 }
